3517ffe708d47b23496610cb950240ded40d2d21
Ren Wang <iamwrm@gmail.com>
2020-04-17 21:35:13 -0400

    c4 finish

---
+++ js/challenge4.js
@@ -0,0 +1,23 @@
+$("form").submit(function (event) {
+
+
+    var name_i = document.getElementById("name")
+    var addr_i = document.getElementById("addr")
+
+
+    console.log(name_i.value)
+    if (name_i.value == "") {
+        $("#nameError").show()
+    } else {
+        $("#nameError").hide()
+    }
+    if (addr_i.value == "") {
+        $("#addrError").show()
+    } else {
+        $("#addrError").hide()
+    }
+    if ((name_i.value == "") || (addr_i.value == "")) {
+        event.preventDefault();
+    }
+
+});
